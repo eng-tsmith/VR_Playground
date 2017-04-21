1dbffa44f56c9083a81ad66d739853e4d7b290df
PolyUCosmos <timothy.smith@3dsnd.com>
2017-04-21 13:43:34 +0800

    Old scripts

---
+++ Ambisonics/WandController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandController : MonoBehaviour {
+	// Grip Button
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	public bool gripButtonDown = false;
+	public bool gripButtonUp = false;
+	public bool gripButtonPressed = false;
+	// Trigger Button
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+	public bool triggerButtonDown = false;
+	public bool triggerButtonUp = false;
+	public bool triggerButtonPressed = false;
+
+	// Controller
+	private SteamVR_Controller.Device controller{ get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+	private SteamVR_TrackedObject trackedObj;
+
+
+
+	// Use this for initialization
+	void Start () {
+		// Get Device ID
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Controller Connected?
+		if (controller == null) {
+			Debug.Log("Controller not initialized");
+		}
+
+		// Get grip button
+		gripButtonDown = controller.GetPressDown(gripButton);
+		gripButtonUp = controller.GetPressUp(gripButton);
+		gripButtonPressed = controller.GetPress(gripButton);
+
+		if(gripButtonDown){
+			Debug.Log("Grip Button pressed");
+		}
+		if(gripButtonUp){
+			Debug.Log("Grip Button released");
+		}
+
+		// Get trigger button
+		triggerButtonDown = controller.GetPressDown(triggerButton);
+		triggerButtonUp = controller.GetPressUp(triggerButton);
+		triggerButtonPressed = controller.GetPress(triggerButton);
+
+		if(triggerButtonDown){
+			Debug.Log("Trigger Button pressed");
+		}
+		if(triggerButtonUp){
+			Debug.Log("Trigger Button released");
+		}
+	}
+}
---
+++ Ambisonics/sendpos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using SharpOSC;
+
+
+using UnityEngine;
+
+public class sendpos : MonoBehaviour {
+	//UDP shit
+	private static string IP_add = "192.168.83.109";
+	private static int port = 9001;
+	private SharpOSC.UDPSender sender = new SharpOSC.UDPSender (IP_add, port);
+
+	// flying sphere
+	GameObject sphere_green;
+
+	// Simple mapping function
+	float map2range(float val, float min_old, float max_old, float min_new, float max_new){
+		return min_new + (val - min_old) * (max_new - min_new) / (max_old - min_old);
+	}
+
+	// Function to send Msg to Linux
+	void sendPos_polar(GameObject pos_arr){
+
+		// x and z for plane, y is height
+		float pos_x = pos_arr.GetComponent<Transform> ().position.x;
+		float pos_y = pos_arr.GetComponent<Transform> ().position.y;
+		float pos_z = pos_arr.GetComponent<Transform> ().position.z;
+
+		float azi;
+		float ele;
+		//Debug.Log(pos_x);
+		//Debug.Log(pos_y);
+		//Debug.Log(pos_z);
+
+		azi = Mathf.Atan2 (pos_x, pos_z)*180/Mathf.PI;
+		ele = Mathf.Atan2(pos_y - 1.6f, Mathf.Sqrt(pos_x*pos_x + pos_z*pos_z))*180/Mathf.PI;
+
+		var msg_azi = new SharpOSC.OscMessage ("/0x00/azi", azi);
+		var msg_ele = new SharpOSC.OscMessage ("/0x00/ele", ele);
+
+		sender.Send(msg_azi);
+		sender.Send(msg_ele);
+	}
+
+
+	// Function to send Msg to Linux
+	void sendPos_cartesian(GameObject pos_arr){
+
+		// x and z for plane, y is height; Unity uses lefthanded system!!!! x to the right y up z to screen
+		float pos_x = pos_arr.GetComponent<Transform> ().position.x;
+		float pos_y = pos_arr.GetComponent<Transform> ().position.y;
+		float pos_z = pos_arr.GetComponent<Transform> ().position.z;
+
+		// OpenAL uses OpenGL right handed system!!!!! transform to x to left y up z to screen
+		var msg_pos = new SharpOSC.OscMessage ("/set_source_position", "sphere", -pos_x, pos_y - 1.6, pos_z);
+
+		sender.Send(msg_pos);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		sphere_green = GameObject.Find ("Sphere_path");
+		sender.Send (new SharpOSC.OscMessage ("/new_source", "sphere"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Found sphere?
+		if (sphere_green == null) {
+			Debug.Log("Sphere not initialized");
+			return;
+		}
+
+		sendPos_cartesian(sphere_green);
+	}
+}
